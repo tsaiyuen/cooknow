46e67eb28c65619b01007b74d9c4da07ef7ea981
Cynthia Tsai <tsaiyuen@gmail.com>
2020-12-01 11:31:42 -0300

    validation changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
-  has_many :recipe_ingredients, :reviews, :saved_recipes
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :saved_recipes, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
 end
