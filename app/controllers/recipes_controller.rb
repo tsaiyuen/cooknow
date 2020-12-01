c800b6bbdc0ca5ff2c063952a58856be6c3bf986
Taro Tomiya <taro.tomiya@gmail.com>
2020-12-01 16:03:46 -0500

    Finished recipe CRUD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,8 +2,7 @@ class RecipesController < ApplicationController
   def index
     if params[:ingredients].present?
       ingredients = params[:ingredients].map(&:to_i)
-      @recipes = Recipe.joins(:ingredients).where(ingredients: {id: params[:ingredients]}).uniq
-      #Recipe.joins(:ingredients).having('ARRAY[?::bigint] = ARRAY_AGG(ingredients.id)', ingredients)
+      @recipes = Recipe.joins(:ingredients).group('recipes.id').having('ARRAY[?::bigint] = ARRAY_AGG(ingredients.id)', params[:ingredients].map(&:to_i))
     else
       redirect_to root_path, alert: "Please insert ingredients"
     end
