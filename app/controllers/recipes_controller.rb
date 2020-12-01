51f460865401ce247cb683b69d5e42e672f0676e
Taro Tomiya <taro.tomiya@gmail.com>
2020-12-01 15:01:01 -0500

    Fixing recipes controller error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,9 @@
 class RecipesController < ApplicationController
   def index
     if params[:ingredients].present?
-      @recipes = Recipe.left_outer_joins(:ingredients).where(ingredients: {id: params[:ingredients]})
+      ingredients = params[:ingredients].map(&:to_i)
+      @recipes = Recipe.joins(:ingredients).where(ingredients: {id: params[:ingredients]}).uniq
+      #Recipe.joins(:ingredients).having('ARRAY[?::bigint] = ARRAY_AGG(ingredients.id)', ingredients)
     else
       redirect_to root_path, alert: "Please insert ingredients"
     end
