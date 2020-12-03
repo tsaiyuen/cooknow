73db49a94e4ff80d8c219390d77dc18e0be4f9d8
Ken Wall <kwall925@gmail.com>
2020-12-03 15:58:29 -0300

    dear god, please shoot me now

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,34 +9,44 @@
 require 'open-uri'
 require 'nokogiri'
 
+p 'deleting recipe ingredients'
+RecipeIngredient.destroy_all
+p 'recipe ingredients deleted'
+p 'deleting  ingredients'
 Ingredient.destroy_all
+p 'ingredients successfully deleted'
+p ' deleting all recipes'
+Recipe.destroy_all
+p 'destroyed all recipes and ingredients'
 
+puts "opening yummly url"
 url = "https://www.yummly.com/recipes"
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
+puts 'successfully launched url'
 
-# recipes = []
-
-html_doc.search('.card-title').each do |element|
+puts 'searching for recipes...'
+html_doc.search('.card-title').5.times do |element|
+  # scrape for title and url of recipe
   title = element.attribute('title').text.strip
-  ingredients_url = element.attribute('href').text.strip
+  ingredients_url = 'https://www.yummly.com' + element.attribute('href').text.strip
   description = 'helllo this is a niiiiiiiiiiiiiice recipe. very nice.'
-  recipe = {
-    title: title,
-    description: description
-  } 
-  newrec = Recipe.create(name: recipe["title"], description: recipe["description"])
-  # recipes << recipe
-  puts newrec
+# recipe_hash = {
+   # name: title,
+   # description: description,
+ # } 
+     Recipe.create(name: title, description: description)
+    # begin scraping for ingredients
+    # search each recipes page
+      ing_html_file = open(ingredients_url).read
+      ing_html_doc = Nokogiri::HTML(ing_html_file)
+     # select IngredientList class and parse ingredients text
+     ing_html_doc.search('.IngredientList').each do |ingElement|
+      ingredient = ingElement.search('.ingredient').text.strip
+      new_ing = Ingredient.create(name: ingredient)
+      recIng = RecipeIngredient.new(ingredient: new_ing, recipe: newrec)
+     end
+  # create recipe ingredients here
+  # add each ingredient to recping
+  # instead of tying to recipe, we assign ingredients to this object
 end
-
-print recipes
-#RecipeIngredient.create(recipe: recipe1, ingredient: ingr01)
-# Recipe.create(name:"Chicken Pie", description:"Lots of chicken, tomato and potato")
-
-# html_file = open(ingredients_url).read
-# html_doc = Nokogiri::HTML(html_file)
-
-# html_doc.search()
-
-# https://www.yummly.com/recipe/Pigs-in-a-Blanket-9057763
