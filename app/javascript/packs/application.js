f355657a0a3ee17c769f7ec8782c287ef25061dc
Taro Tomiya <taro.tomiya@gmail.com>
2020-12-03 16:53:40 -0500

    Muuri

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,8 +28,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initSelect2 } from '../components/init_select2';
+import { muuri } from '../components/muuri';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initSelect2();
+  muuri();
 });
