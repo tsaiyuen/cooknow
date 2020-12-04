3acaa3157094b0f5beaea49fd603d1245643795c
Taro Tomiya <taro.tomiya@gmail.com>
2020-12-04 12:12:09 -0500

    pending

--- app/javascript/components/muuri.js
+++ app/javascript/components/muuri.js
@@ -1,9 +1,18 @@
 const muuri = () => {
   const grid = new Muuri('.grid', {dragEnabled: true});
   //document.querySelector('dropdown-item')
-  document.getElementById('testinput').addEventListener('change', (event) => {
+  document.getElementById('searchinput').addEventListener('change', (event) => {
     grid.filter(`.${event.target.value}`);
   });
+
+  //document.querySelector('.dropdown-menu').addEventListener('click', (event) => {
+  //  grid.filter(`.${event.target.value}`);
+  //});
+
+ // $(".dropdown-menu").click(function(){
+  //var selText = $(this).text();
+  //$(this).parents('.form-group').find('button[data-toggle="dropdown"]').html(selText+' <span class="caret"></span>');
+//});
 }
 //const filterField = document.querySelector('.grid-control-field.filter-field');
 //filterField.addEventListener('change', filter);
