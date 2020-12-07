6f74e5b6ef04dafcce4308053245ac8230cb3135
Taro Tomiya <taro.tomiya@gmail.com>
2020-12-07 23:49:08 +0900

    star rating system

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,10 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initSelect2 } from '../components/init_select2';
 import { muuri } from '../components/muuri';
+import { initStarRating } from '../plugins/init_star_rating';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
+  initStarRating();
   initSelect2();
   muuri();
 });
