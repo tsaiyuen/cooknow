dcc8c11784f24e914e923e0ac8d3620ff4a9ecdb
Cynthia Tsai <tsaiyuen@gmail.com>
2020-12-07 17:37:51 -0300

    Seeds working for recipe, ingredient list and RecipeIngredient.

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,6 @@ request["x-rapidapi-key"] = ENV["RAPIDAPI_KEY"]
 response = http.request(request)
 api_res = JSON.parse(response.read_body)
 
-# we will need to include recipes loop within this loop, so that ingredients also belong to their parent recipes
 api_res["results"].each do |recipe|
   puts "Creating recipes..."
   name = recipe["name"]
@@ -43,38 +42,27 @@ api_res["results"].each do |recipe|
   recipe["sections"][0]["components"].each do |ingredient|
     puts "creating ingredient"
     ingName = ingredient["ingredient"]["name"]
-    puts ingName
-
-    puts "creating our ingredient"
+ 
+    puts "creating Ingredient DB"
     ourIngredient = Ingredient.where(name: ingName).first
     if ourIngredient.nil?
       ourIngredient = Ingredient.create(name: ingName)
     end  
-    puts ourIngredient
     
-    puts "creating our recIng"
+    puts "creating our Recipe Ingredient"
     recIng = RecipeIngredient.create(recipe: ourRecipe, ingredient: ourIngredient)
   end
  
 end
 
-#api_res["results"].each do |recipe|
-# next if recipe["sections"].nil?
-# recipe["sections"][0]["components"].each do |ingredient|
-#  ingName = ingredient["ingredient"]["name"]
-#  ourIngredient = Ingredient.create(name: ingName)
-#  # puts ourIngredient.name
-# end
-#end
-
-#puts "Creating users..."
-#user1 = User.create(email:"cynthia@gmail.com", first_name:"Cynthia", last_name:"Tong", password:"123456", phone_number: "123123")
-#file1 = open('https://avatars0.githubusercontent.com/u/67703828?v=4')
-#user1.photo.attach(io: file1, filename: 'some-image1.jpg')
-#user2 = User.create(email:"taro@gmail.com", first_name:"Taro", last_name:"Tomiya", password:"123456", phone_number: "1211")
-#file2 = open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1602512708/wpr8u0xszuxl3kvd6l57.jpg')
-#user2.photo.attach(io: file2, filename: 'some-image2.jpg')
-#user3 = User.create(email:"ken@gmail.com", first_name:"Ken", last_name:"Wall", password:"123456", phone_number: "22222")
-#file3 = open('https://avatars3.githubusercontent.com/u/51733007?v=4')
-#user3.photo.attach(io: file3, filename: 'some-image3.jpg')
-#puts "Finished creating user!"
+puts "Creating users..."
+user1 = User.create(email:"cynthia@gmail.com", first_name:"Cynthia", last_name:"Tong", password:"123456", phone_number: "123123")
+file1 = open('https://avatars0.githubusercontent.com/u/67703828?v=4')
+user1.photo.attach(io: file1, filename: 'some-image1.jpg')
+user2 = User.create(email:"taro@gmail.com", first_name:"Taro", last_name:"Tomiya", password:"123456", phone_number: "1211")
+file2 = open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1602512708/wpr8u0xszuxl3kvd6l57.jpg')
+user2.photo.attach(io: file2, filename: 'some-image2.jpg')
+user3 = User.create(email:"ken@gmail.com", first_name:"Ken", last_name:"Wall", password:"123456", phone_number: "22222")
+file3 = open('https://avatars3.githubusercontent.com/u/51733007?v=4')
+user3.photo.attach(io: file3, filename: 'some-image3.jpg')
+puts "Finished creating user!"
