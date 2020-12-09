7ca8defa780f42aa2be4836ec9aa0a406438aeec
Cynthia Tsai <tsaiyuen@gmail.com>
2020-12-09 15:58:54 -0300

    adding extra seed and solving conflits. Need to check footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,22 +78,22 @@ RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_b
 RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Carrot"))
 
 recipe2 = Recipe.create(name: "Baked Chicken Tikka", description: "This is the famous Chicken Tikka Style - Chicken marinated, grilled or baked in Conventional Oven Or traditional Indian Tandoor. Serve it as Appetizer with onion and Green Chutney of your choice. And this will taste great with some exta ingredietns like potato and carrots!", image: "https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/recipes/f62e2e5a3dc443dc9caed4d9894aa8c0.jpeg?output-format=webp&output-quality=auto", video_url: "", instructions: ["Combine all ingredients in a bowl except chicken.", "Make long slits on chicken with a knife.", "Put the chicken in the marinade overnight.", "Bake at 450°F (230°C) for 50 minutes..", "Enjoy!"])
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Potato"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Chicken"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Garlic"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Carrot"))
+RecipeIngredient.create(recipe: recipe2, ingredient: Ingredient.find_or_create_by(name: "Potato"))
+RecipeIngredient.create(recipe: recipe2, ingredient: Ingredient.find_or_create_by(name: "Chicken"))
+RecipeIngredient.create(recipe: recipe2, ingredient: Ingredient.find_or_create_by(name: "Garlic"))
+RecipeIngredient.create(recipe: recipe2, ingredient: Ingredient.find_or_create_by(name: "Carrot"))
 
 recipe3 = Recipe.create(name: "Honey BBQ Chicken Wings", description: "Honey BBQ Chicken Wings is so juicy and flavourful... They’re crisp and then tossed in Honey BBQ Sauce and covered in a blend of spices. ", image: "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/4c41b09cf41644a7b7ca1780edbeb960/BFV8354_Honey_BBQ_Chicken_Wings-FB1080.jpg?", video_url: "https://vid.tasty.co/output/31936/mp4_640x640/1495124926", instructions: ["Preheat oven to 425°F (220°C).", "In a bowl, combine flour, chili powder, salt, pepper, paprika, and garlic powder. Coat the wings in the flour evenly, shaking off any excess.", "Place the floured wings on a parchment paper–lined baking sheet and spread them out in a single layer.", "Bake for 45 minutes, flipping halfway through, until skin is crispy and golden brown.", "In a separate bowl, combine the BBQ sauce and the honey. Stir the cooked wings in the sauce and coat them evenly.", "Place the coated wings back onto the baking sheet and spread them in a single layer.", "Bake for 8 to 10 minutes, until sauce is bubbly and caramelized. Cool, then serve.", "Enjoy!"])
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Potato"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Chicken"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Garlic"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Carrot"))
+RecipeIngredient.create(recipe: recipe3, ingredient: Ingredient.find_or_create_by(name: "Potato"))
+RecipeIngredient.create(recipe: recipe3, ingredient: Ingredient.find_or_create_by(name: "Chicken"))
+RecipeIngredient.create(recipe: recipe3, ingredient: Ingredient.find_or_create_by(name: "Garlic"))
+RecipeIngredient.create(recipe: recipe3, ingredient: Ingredient.find_or_create_by(name: "Carrot"))
 
 recipe4 = Recipe.create(name: "Chicken Rollups", description: "You will not want anything else after enjoying this wonderful dish! It is easy and practical and very delicious! Everyone will love it!", image: "https://img.buzzfeed.com/video-api-prod/assets/65b4d49bec3d4483a6f6160d812c5333/BFV6250_BBQChickenRollulps-Thumb1080.jpg?", video_url: "https://vid.tasty.co/output/32109/mp4_640x640/1495125366", instructions: ["Preheat oven to 425°F (220°C).", "Roll out the pizza crust into a rectangle.", "Spread on the BBQ sauce. Top with chicken, onion, cheese, and parsley.", "Roll it up longways, and cut into slices. Bake face-up for 15 minutes.", "Enjoy!"])
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Potato"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Chicken"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Garlic"))
-RecipeIngredient.create(recipe: recipe1, ingredient: Ingredient.find_or_create_by(name: "Carrot"))
+RecipeIngredient.create(recipe: recipe4, ingredient: Ingredient.find_or_create_by(name: "Potato"))
+RecipeIngredient.create(recipe: recipe4, ingredient: Ingredient.find_or_create_by(name: "Chicken"))
+RecipeIngredient.create(recipe: recipe4, ingredient: Ingredient.find_or_create_by(name: "Garlic"))
+RecipeIngredient.create(recipe: recipe4, ingredient: Ingredient.find_or_create_by(name: "Carrot"))
 
 puts "Finished creating extra recipes"
 
